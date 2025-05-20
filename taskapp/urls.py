50621a17e7148f321102a7e6736740cd6900e95b
Anurag <kpanurag2002@gmail.com>
2025-05-20 21:48:57 +0530

    <>|Taskupdate api created|<>

--- taskapp/urls.py
+++ taskapp/urls.py
@@ -5,5 +5,8 @@ from .views import*
 
 
 urlpatterns=[
-    path('GettaskApiView/',GettaskApiView.as_view(),name='GettaskApiView')
+    path('task/',GettaskApiView.as_view(),name='GettaskApiView'),
+    path('task/<int:id>',TaskupdateView.as_view(),name='TaskupdateView'),
+    path('tasks/<int:id>/report/', TaskReportView.as_view(), name='TaskReportView')
+    
 ]
